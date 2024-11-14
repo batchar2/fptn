d30bbb07caa554de4950387bf40b85f33a99c345
Stas Skokov <7090stas@gmail.com>
2024-11-14 23:17:39 +1100

    tmp commit

--- src/android/app/src/main/java/org/fptn/client/service/vpn/FptnVpnService.java
+++ src/android/app/src/main/java/org/fptn/client/service/vpn/FptnVpnService.java
@@ -17,32 +17,12 @@ import java.nio.ByteBuffer;
 import java.util.concurrent.Executors;
 
 
-//public class FptnVpnService extends Service {
-//    private static final String LOG_TAG = "FptnVpnService";
-//
-//    @Nullable
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//        Log.i(LOG_TAG, "onBind++");
-//        return null;
-//    } //VpnService implements Runnable {
-//
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        Log.i(LOG_TAG, "onCreate++");
-//    }
-//
-//    @Override
-//    public int onStartCommand(Intent intent, int flags, int startId) {
-//        Log.i(LOG_TAG, "onStartCommand++");
-//        return super.onStartCommand(intent, flags, startId);
-//    }
-//
-//}
 
-public class FptnVpnService extends VpnService implements Runnable {
+public class FptnVpnService extends VpnService { //implements Runnable {
 //    private static FptnVpnService instance = null; // Singleton instance
+
+    private static final String VPN_ADDRESS = "10.10.10.1"; // Only IPv4 support for now
+    private static final String VPN_ROUTE = "0.0.0.0"; // Intercept everything
 
 
     private static final String LOG_TAG = "FptnVpnService";
@@ -82,25 +62,43 @@ public class FptnVpnService extends VpnService implements Runnable {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.i(TAG, "onCreateFptnVpnService");
+    }
+
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(LOG_TAG, "onStartCommand++");
-        String dnsServer = intent.getStringExtra("dnsServer");
+        Log.i(TAG, "onStartCommand");
+        try {
+            Log.i(LOG_TAG, "onStartCommand++");
+            String dnsServer = intent.getStringExtra("dnsServer");
 //        if (instance == null) {
 //            instance = this;
 //        }
-        Builder builder = new Builder();
-        builder.setSession("FptnVPN")
-                .addAddress("10.10.10.1", 24)
-                .addRoute("0.0.0.0", 0)
-                .addDnsServer(dnsServer)
-                .setMtu(MTU_SIZE);
-        vpnInterface = builder.establish();
-        if (vpnInterface != null) {
-            Log.i(LOG_TAG, "onStartCommand++START");
-            thread = new Thread(this);
-            thread.start();
-        } else {
-            Log.i(LOG_TAG, "onStartCommand++EMPTY");
+            Builder builder = new Builder();
+            builder //.setSession("FptnVPN")
+                    .addAddress(VPN_ADDRESS, 24)
+                    .addRoute(VPN_ROUTE, 0)
+
+                    ;
+//                    .addDnsServer(dnsServer)
+//                    .setMtu(MTU_SIZE);
+
+//            builder.addAllowedApplication("org.fptn.client.service.vpn");
+
+            vpnInterface = builder.establish();
+            if (vpnInterface != null) {
+                Log.i(LOG_TAG, "onStartCommand++START");
+//                thread = new Thread(this);
+//                thread.start();
+            } else {
+                Log.i(LOG_TAG, "onStartCommand++EMPTY");
+            }
+        } catch (Exception err) {
+            Log.e(LOG_TAG, "Exception: " + err.getMessage());
         }
         return START_STICKY;
     }
@@ -115,34 +113,34 @@ public class FptnVpnService extends VpnService implements Runnable {
         super.onDestroy();
     }
 
-    @Override
-    public void run() {
-        try {
-            FileInputStream in = new FileInputStream(vpnInterface.getFileDescriptor());
-            out = new FileOutputStream(vpnInterface.getFileDescriptor());
-
-            ByteBuffer packetBuffer = ByteBuffer.allocate(MTU_SIZE);
-            while (!Thread.interrupted()) {
-                packetBuffer.clear();
-                int length = in.read(packetBuffer.array());
-
-                if (length > 0) {
-                    byte[] packetData = new byte[length];
-                    packetBuffer.get(packetData, 0, length);
-
-                    // Callback to send packet to client
-                    callback.onMessageReceived(packetData);
-
-                    // Echo packet back to output stream
-                    out.write(packetData, 0, length);
-                }
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "Error processing VPN packets", e);
-        } finally {
-            stopVpn();
-        }
-    }
+//    @Override
+//    public void run() {
+//        try {
+//            FileInputStream in = new FileInputStream(vpnInterface.getFileDescriptor());
+//            out = new FileOutputStream(vpnInterface.getFileDescriptor());
+//
+//            ByteBuffer packetBuffer = ByteBuffer.allocate(MTU_SIZE);
+//            while (!Thread.interrupted()) {
+//                packetBuffer.clear();
+//                int length = in.read(packetBuffer.array());
+//
+//                if (length > 0) {
+//                    byte[] packetData = new byte[length];
+//                    packetBuffer.get(packetData, 0, length);
+//
+//                    // Callback to send packet to client
+//                    callback.onMessageReceived(packetData);
+//
+//                    // Echo packet back to output stream
+//                    out.write(packetData, 0, length);
+//                }
+//            }
+//        } catch (Exception e) {
+//            Log.e(TAG, "Error processing VPN packets", e);
+//        } finally {
+//            stopVpn();
+//        }
+//    }
 
     public boolean send(byte[] msg) {
         try {
