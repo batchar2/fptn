b9633cb33ee4d498d9e7b6badb638710ca041ec1
Stas Skokov <7090stas@gmail.com>
2024-11-17 08:00:58 +1100

    fix protobuf

--- src/android/app/src/main/java/org/fptn/client/service/vpn/VpnServiceCallback.java
+++
@@ -1,6 +0,0 @@
-package org.fptn.client.service.vpn;
-
-public interface VpnServiceCallback {
-    void onMessageReceived(byte[] msg);
-    void onError(Exception e);
-}
--- src/android/app/src/main/java/org/fptn/client/service/websocket/WebSocketCallback.java
+++
@@ -1,8 +0,0 @@
-package org.fptn.client.service.websocket;
-
-import com.google.protobuf.ByteString;
-
-public interface WebSocketCallback {
-    void onMessageReceived(byte[] msg);
-    void onError(Exception e);
-}
