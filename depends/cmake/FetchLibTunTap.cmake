69161df53d5e9872be79a0beff9a613850ae988e
Stas Skokov <7090stas@gmail.com>
2025-04-15 18:09:03 +1000

    .cmake-format file (#34)

--- depends/cmake/FetchLibTunTap.cmake
+++ depends/cmake/FetchLibTunTap.cmake
@@ -19,10 +19,7 @@ set(LIBTUNTAP_DISABLE_TESTS
     CACHE BOOL "Disable library tests" FORCE)
 
 include(FetchContent)
-FetchContent_Declare(
-  Libtuntap
-  URL https://github.com/LaKabane/libtuntap/archive/ec1213733eb2e66e033ff8864d9fd476f9e35ffe.zip
-)
+FetchContent_Declare(Libtuntap URL https://github.com/LaKabane/libtuntap/archive/ec1213733eb2e66e033ff8864d9fd476f9e35ffe.zip)
 FetchContent_GetProperties(Libtuntap)
 if(NOT Libtuntap_POPULATED)
   FetchContent_Populate(Libtuntap)
@@ -32,8 +29,7 @@ set(Libtuntap_SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/libtuntap-src")
 set(Libtuntap_BINARY_DIR "${CMAKE_BINARY_DIR}/_deps/libtuntap-build")
 set(Libtuntap_INCLUDE_DIR "${Libtuntap_BINARY_DIR}/include")
 
-add_subdirectory("${Libtuntap_SOURCE_DIR}" "${Libtuntap_BINARY_DIR}"
-                 EXCLUDE_FROM_ALL)
+add_subdirectory("${Libtuntap_SOURCE_DIR}" "${Libtuntap_BINARY_DIR}" EXCLUDE_FROM_ALL)
 
 include_directories("${Libtuntap_SOURCE_DIR}/libtuntap/")
 include_directories("${Libtuntap_SOURCE_DIR}/libtuntap/bindings/cpp")
--- depends/cmake/FetchWintun.cmake
+++ depends/cmake/FetchWintun.cmake
@@ -9,12 +9,10 @@ endif()
 
 set(Wintun_INCLUDE_DIR "${wintun_SOURCE_DIR}/include")
 
-if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" OR CMAKE_SYSTEM_PROCESSOR MATCHES
-                                              "AMD64")
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
   set(Wintun_REDISTRIBUTABLE "${wintun_SOURCE_DIR}/bin/amd64/wintun.dll")
   set(Wintun_REDISTRIBUTABLE_DIR "${wintun_SOURCE_DIR}/bin/amd64")
-elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "arm64" OR CMAKE_SYSTEM_PROCESSOR MATCHES
-                                                 "aarch64")
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "arm64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
   set(Wintun_REDISTRIBUTABLE "${wintun_SOURCE_DIR}/bin/arm64/wintun.dll")
   set(Wintun_REDISTRIBUTABLE_DIR "${wintun_SOURCE_DIR}/bin/arm64")
 else()
