0da3f9fc49c53013cd146879b548ebc187a639ea
Stas Skokov <7090stas@gmail.com>
2025-04-23 17:07:01 +1000

    fix CI

--- .conan/recipes/boringssl/conanfile.py
+++ .conan/recipes/boringssl/conanfile.py
@@ -12,14 +12,17 @@ class BoringSSLConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    requires = (
-        "zlib/1.3.1",
-    )
+    requires = ("zlib/1.3.1",)
 
     def source(self):
         url = "https://github.com/batchar2/boringssl/archive/refs/heads/main.tar.gz"
         get(self, url, strip_root=False)
-        copy(self, "*", src=(self.source_folder + "/boringssl-main"), dst=self.source_folder)
+        src = self.source_folder + "/boringssl-main"
+        copy(self, "*", src=src, dst=self.source_folder)
+
+    def build_requirements(self):
+        if self.settings.os == "Windows":
+            self.requires("nasm/2.16.01", override=True, force=True)
 
     def build(self):
         cmake = CMake(self)
@@ -41,5 +44,9 @@ class BoringSSLConan(ConanFile):
 
         # for CMake find_package(OpenSSL)
         self.cpp_info.set_property("cmake_file_name", "OpenSSL")
-        self.cpp_info.components["ssl"].set_property("cmake_target_name", "OpenSSL::SSL")
-        self.cpp_info.components["crypto"].set_property("cmake_target_name", "OpenSSL::Crypto")
+        self.cpp_info.components["ssl"].set_property(
+            "cmake_target_name", "OpenSSL::SSL"
+        )
+        self.cpp_info.components["crypto"].set_property(
+            "cmake_target_name", "OpenSSL::Crypto"
+        )
