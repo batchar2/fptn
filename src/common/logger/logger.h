4da217740add51f333b1b3d4c0c273c31c74358d
Stas Skokov <7090stas@gmail.com>
2025-05-30 18:14:56 +1000

    Macos Intel (#56)
    * intel mac
    
    * fix logger and cmake

--- src/common/logger/logger.h
+++ src/common/logger/logger.h
@@ -34,7 +34,7 @@ inline bool init(const std::string& app_name) {
 #endif
     // Set locale
     std::locale::global(std::locale::classic());
-    std::setlocale(LC_ALL, "C");
+    setlocale(LC_ALL, "C");
 
     const std::filesystem::path log_file = log_dir / (app_name + ".log");
     if (!std::filesystem::exists(log_dir)) {
