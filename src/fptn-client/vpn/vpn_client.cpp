28d4fc05828393aba13ec64a35221ad593373fa2
Stas Skokov <7090stas@gmail.com>
2025-11-12 11:27:18 +1100

    [bugfix] fix client close lock

--- src/fptn-client/vpn/vpn_client.cpp
+++ src/fptn-client/vpn/vpn_client.cpp
@@ -26,10 +26,6 @@ VpnClient::VpnClient(fptn::vpn::http::ClientPtr http_client,
 VpnClient::~VpnClient() { Stop(); }
 
 bool VpnClient::IsStarted() {
-  if (!running_) {
-    return false;
-  }
-
   const std::unique_lock<std::mutex> lock(mutex_);  // mutex
 
   return running_ && http_client_ && http_client_->IsStarted();
@@ -39,11 +35,14 @@ bool VpnClient::Start() {
   if (running_) {
     return false;
   }
-  const std::unique_lock<std::mutex> lock(mutex_);  // mutex
 
-  // cppcheck-suppress identicalConditionAfterEarlyExit
-  if (running_) {
-    return false;
+  {
+    const std::unique_lock<std::mutex> lock(mutex_);  // mutex
+
+    // cppcheck-suppress identicalConditionAfterEarlyExit
+    if (running_) {
+      return false;
+    }
   }
 
   // NOLINTNEXTLINE(modernize-avoid-bind)
@@ -65,6 +64,7 @@ bool VpnClient::Stop() {
   if (!running_) {
     return false;
   }
+
   const std::unique_lock<std::mutex> lock(mutex_);  // mutex
 
   // cppcheck-suppress identicalConditionAfterEarlyExit
@@ -73,19 +73,21 @@ bool VpnClient::Stop() {
   }
 
   running_ = false;
+
   SPDLOG_INFO("Stopping VPN client...");
+
+  if (virtual_net_interface_) {
+    SPDLOG_INFO("Stopping virtual network interface");
+    virtual_net_interface_->Stop();
+    virtual_net_interface_.reset();
+    SPDLOG_DEBUG("Virtual network interface stopped successfully");
+  }
 
   if (http_client_) {
     SPDLOG_INFO("Stopping HTTP client");
     http_client_->Stop();
     http_client_.reset();
     SPDLOG_DEBUG("HTTP client stopped successfully");
-  }
-  if (virtual_net_interface_) {
-    SPDLOG_INFO("Stopping virtual network interface");
-    virtual_net_interface_->Stop();
-    virtual_net_interface_.reset();
-    SPDLOG_DEBUG("Virtual network interface stopped successfully");
   }
   return true;
 }
