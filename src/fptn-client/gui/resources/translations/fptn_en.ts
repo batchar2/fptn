9cbfdb3ac88ac274c4c571884687482a2ea1aa23
Stas Skokov <7090stas@gmail.com>
2026-02-08 21:01:41 +1100

    [feture] fix links (#217)

--- src/fptn-client/gui/resources/translations/fptn_en.ts
+++ src/fptn-client/gui/resources/translations/fptn_en.ts
@@ -234,7 +234,7 @@
             <![CDATA[Join our Telegram communities:
                 <br/> - <a href="https://t.me/fpnt_project_global">English Community</a>
                 <br/> - <a href="https://t.me/fptn_project">Russian Community</a>
-                <br/> - <a href="t.me/fptn_project_iran">Persian Community</a>
+                <br/> - <a href="https://t.me/fptn_project_iran">Persian Community</a>
             ]]>
             </translation>
         </message>
